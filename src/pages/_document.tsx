5f30a02c5d95e66cbd7cb48f14469a94d6d333a7
nekko1119 <taku50501119@gmail.com>
2021-05-05 19:34:37 +0900

    feat: add styled-components and rebass

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+  
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  render() {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
+
+    return (
+      <Html>
+        <Head>{styleTags}</Head>
+        <body>
+          <div className="root">{main}</div>
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument;
