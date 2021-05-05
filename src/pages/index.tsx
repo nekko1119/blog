500040d712877e0d5152db3850d44a97c1423311
nekko1119 <taku50501119@gmail.com>
2021-05-05 16:53:26 +0900

    feat: enumerate pages on top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import type { GetStaticProps } from "next";
+import Link from "next/link";
+import { Post, getPostPathsAll, getPost, getFilename } from "../domains/posts";
 
-const Main: React.FC = () => {
-  return <div>test</div>;
-}
+type Props = {
+  posts: Array<Post>;
+};
+
+const Main: React.FC<Props> = ({ posts }) => {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.meta.title ?? post.meta.path}>
+          <Link href={{ pathname: post.meta.path }}>
+            <a>{post.meta.title ?? post.meta.path}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const paths = await getPostPathsAll();
+  const posts = await Promise.all(paths.map(async (path) => await getPost(getFilename(path))));
+
+  return { props: { posts } };
+};
 
 export default Main;
